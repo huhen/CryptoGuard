89584a22f2efe87cc5c5545f3b8ee1bca583aedc
huhen <kinetic@bk.ru>
2025-05-07 13:56:21 +0000

    Tests for decrypt

--- tests/crypo_guard_ctx_test.cpp
+++ tests/crypo_guard_ctx_test.cpp
@@ -66,3 +66,98 @@ TEST(CryptoGuardCtx, EncryptBadOutput) {
 
     ASSERT_THROW(ctx.EncryptFile(istream, ostream, "1234"), std::runtime_error);
 }
+
+TEST(CryptoGuardCtx, DecryptBadInput) {
+    CryptoGuard::CryptoGuardCtx ctx;
+    std::stringstream istream{"12341234"};
+    std::stringstream estream;
+    std::stringstream ostream;
+
+    ctx.EncryptFile(istream, estream, "1234");
+
+    estream.setstate(std::ios::failbit);
+
+    ASSERT_THROW(ctx.DecryptFile(estream, ostream, "1234"), std::runtime_error);
+}
+
+TEST(CryptoGuardCtx, DecryptBadOutput) {
+    CryptoGuard::CryptoGuardCtx ctx;
+    std::stringstream istream{"12341234"};
+    std::stringstream estream;
+    std::stringstream ostream;
+
+    ctx.EncryptFile(istream, estream, "1234");
+
+    ostream.setstate(std::ios::badbit);
+
+    ASSERT_THROW(ctx.DecryptFile(estream, ostream, "1234"), std::runtime_error);
+}
+
+TEST(CryptoGuardCtx, DecryptEmptyInput) {
+    CryptoGuard::CryptoGuardCtx ctx;
+    std::stringstream istream{""};
+    std::stringstream ostream;
+
+    ASSERT_THROW(ctx.DecryptFile(istream, ostream, "1234"), std::runtime_error);
+}
+
+TEST(CryptoGuardCtx, DecryptEmptyPassword) {
+    CryptoGuard::CryptoGuardCtx ctx;
+    std::stringstream istream{"12341234"};
+    std::stringstream estream;
+    std::stringstream ostream;
+
+    ctx.EncryptFile(istream, estream, "");
+
+    ctx.DecryptFile(estream, ostream, "");
+
+    EXPECT_EQ(istream.str(), ostream.str());
+}
+
+TEST(CryptoGuardCtx, DecryptShortInput) {
+    CryptoGuard::CryptoGuardCtx ctx;
+    std::stringstream istream{"12341234"};
+    std::stringstream estream;
+    std::stringstream ostream;
+
+    ctx.EncryptFile(istream, estream, "1234");
+
+    ctx.DecryptFile(estream, ostream, "1234");
+
+    EXPECT_EQ(istream.str(), ostream.str());
+}
+
+TEST(CryptoGuardCtx, DecryptLongInput) {
+    CryptoGuard::CryptoGuardCtx ctx;
+    std::stringstream istream;
+    std::stringstream estream;
+    std::stringstream ostream;
+
+    for (int i = 0; i != 4096; ++i) {
+        istream << "12341234";
+    }
+
+    ctx.EncryptFile(istream, estream, "1234");
+
+    ctx.DecryptFile(estream, ostream, "1234");
+
+    EXPECT_EQ(istream.str(), ostream.str());
+}
+
+TEST(CryptoGuardCtx, DecryptWrongPassword) {
+    CryptoGuard::CryptoGuardCtx ctx;
+    std::stringstream istream{"12341234"};
+    std::stringstream estream;
+    std::stringstream ostream;
+
+    ctx.EncryptFile(istream, estream, "1234");
+
+    ASSERT_THROW(
+        {
+            ctx.DecryptFile(estream, ostream, "1233");
+            if (istream.str() != ostream.str()) {
+                throw std::runtime_error("Password mismatch");
+            }
+        },
+        std::runtime_error);
+}
